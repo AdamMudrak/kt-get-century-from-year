7cbc9386f2e4baccde2b84423c71f0d229f204d5
AdamMudrak <mudrets2000@gmail.com>
2025-07-21 22:16:41 +0300

    Add solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+const val CENTURY_BALANCER = 1
+const val YEARS_IN_CENTURY  = 100
+const val CENTURY_OFFSET  = 1
+
 fun getCentury(year: Int) : Int {
-    return 0
+    return (year - CENTURY_BALANCER) / YEARS_IN_CENTURY + CENTURY_OFFSET
 }
